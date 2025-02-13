56f7582c1f55e0c40a8718ba9accdd67c3f9e99d
paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-02-13 13:07:49 +0530

    Change the driver to gorm

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,38 +1,54 @@
 package config
 
 import (
-	"context"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-// Connect establishes a connection to the PostgreSQL database
-func Connect() (*pgx.Conn, error) {
-	host := "localhost"
-	port := 5432
-	user := "postgres"
-	password := "root"
-	dbname := "ride_service"
+var DB *gorm.DB
 
-	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user, password, host, port, dbname)
-
-	// Establish the connection
-	conn, err := pgx.Connect(context.Background(), dbURL)
+// Connection
+func Connect() {
+	err := godotenv.Load(".env")
 	if err != nil {
-		return nil, fmt.Errorf("unable to connect to database: %v", err)
+		log.Fatalf("Error loading .env file, %s", err)
 	}
 
-	log.Println("Connected to PostgreSQL......")
-	return conn, nil
+	// Get database configuration details from environment variables
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
+
+	var errOpen error
+	DB, errOpen = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if errOpen != nil {
+		log.Fatalf("Could not connect to the database: %v", errOpen)
+	}
+
+	fmt.Println("Successfully connected to PostgreSQL database successfully.........")
 }
 
-func Disconnect(conn *pgx.Conn) {
-	err := conn.Close(context.Background())
+func Disconnect() {
+	sqlDB, err := DB.DB()
 	if err != nil {
-		log.Printf("Error closing connection: %v", err)
-	} else {
-		log.Println("Disconnected from PostgreSQL")
+		log.Fatalf("Could not get raw DB object: %v", err)
 	}
+
+	err = sqlDB.Close()
+	if err != nil {
+		log.Fatalf("Error closing the database connection: %v", err)
+	}
+
+	fmt.Println("Database connection closed!")
 }
